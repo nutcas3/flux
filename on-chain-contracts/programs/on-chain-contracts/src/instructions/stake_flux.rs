ad8f5f8f5c469157d35b3a4abe5f9883b4ae43a8
nutcas3 <maurice.nyanja.e@gmail.com>
2025-09-29 18:39:32 +0300

    feat: implement SPL token transfer for FLUX staking via CPI

--- on-chain-contracts/programs/on-chain-contracts/src/instructions/stake_flux.rs
+++ on-chain-contracts/programs/on-chain-contracts/src/instructions/stake_flux.rs
@@ -1,10 +1,17 @@
 use pinocchio::{
     account_info::AccountInfo,
+    instruction::{AccountMeta, Instruction},
+    msg,
+    program::invoke,
     program_error::ProgramError,
+    pubkey::Pubkey,
     ProgramResult,
 };
 
 use crate::state;
+
+
+const SPL_TOKEN_PROGRAM_ID: Pubkey = pubkey!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
 
 pub fn stake_flux(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
     let host = &accounts[0];
@@ -23,11 +30,32 @@ pub fn stake_flux(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
         return Err(ProgramError::Custom(2));
     }
 
-    // Simplified: Assume token transfer to staking pool
-    // In reality, use SPL Token CPI for staking
-    msg!("Staking {} FLUX for host {}", amount, host.key);
+    let (staking_pda, bump) = Pubkey::create_program_address(
+        &[b"stake", resource_account.key.as_ref()],
+        &pinocchio::ID,
+    )?;
+    let transfer_ix = spl_token::instruction::transfer(
+        &SPL_TOKEN_PROGRAM_ID,
+        token_account.key,
+        &staking_pda,
+        host.key,
+        &[],
+        amount,
+    )?;
 
-    // Update staked amount in resource
+    let transfer_instruction = Instruction {
+        program_id: *token_program.key,
+        accounts: vec![
+            AccountMeta::new(*token_account.key, false),
+            AccountMeta::new(staking_pda, false),
+            AccountMeta::new(*host.key, true),
+        ],
+        data: transfer_ix.data,
+    };
+
+    invoke(&transfer_instruction, accounts)?;
+
+    msg!("Staked {} FLUX for host {}", amount, host.key);
     let mut resource_data_mut = resource_account.try_borrow_mut_data()?;
     let mut resource_mut = state::ResourceAccount::try_from_slice(&resource_data_mut)?;
     resource_mut.staked_flux += amount;
