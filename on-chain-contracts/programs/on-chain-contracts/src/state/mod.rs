92d5c14abbe0ff91d8e1c9dcbf01acc80b05f7a9
nutcas3 <maurice.nyanja.e@gmail.com>
2025-09-29 23:13:48 +0300

    refactor: remove proposal system and related functionality

--- on-chain-contracts/programs/on-chain-contracts/src/state/mod.rs
+++ on-chain-contracts/programs/on-chain-contracts/src/state/mod.rs
@@ -1,11 +1,9 @@
 pub mod resource;
 pub mod job;
 pub mod escrow;
-pub mod proposal;
 pub mod error;
 
 pub use resource::{ResourceAccount, ResourceSpecs, ResourceStatus};
 pub use job::{JobAccount, JobStatus};
 pub use escrow::{EscrowAccount, EscrowStatus};
-pub use proposal::{ProposalAccount, ProposalStatus};
 pub use error::FluxError;
